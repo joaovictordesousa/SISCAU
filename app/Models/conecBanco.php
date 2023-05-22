450b8db71ef4e5c5a6ae9030b934d9e7fa393b2b
joaovictodesousa <joaovictorsr26@gmail.com>
2023-05-22 10:04:59 -0300

    att

---
+++ app/Models/conecBanco.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class conecBanco extends Model
+ {
+//     protected $table = 'sua_tabela';
+//     protected $connection = 'sqlsrv';
+ }
