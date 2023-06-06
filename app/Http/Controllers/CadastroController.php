3bdde8bd144b53e96358667b54622c5d7c8ce01d
joaovictodesousa <joaovictorsr26@gmail.com>
2023-06-06 11:22:11 -0300

    att

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Cadastro;
+use App\Models\GuiasRecolhimento;
 
 class CadastroController extends Controller
 {
@@ -17,40 +18,44 @@ class CadastroController extends Controller
     {
         // Validação dos dados recebidos do formulário
         $request->validate([
-            'recolhimento' => 'required',
-            'financeira' => 'required',
-            'agencias' => 'required',
-            'contas' => 'required',
-            'contatos' => 'required',
-            'aditivos' => 'required',
-            'datas_grs' => 'required',
-            'data_validade' => 'required',
-            'documentos' => 'required',
-            'numeros' => 'required',
-            'empresa' => 'required',
-            'valores' => 'required',
-            'numeros_nls' => 'required',
-            'historicos' => 'required',
+            'auxtiporecolhimentoid' => 'required',
+            'auxinstituicaofinanceiraid' => 'required',
+            'auxagenciaid' => 'required',
+            'auxempresaid' => 'required',
+            'datagr' => 'required',
+            'datavalidade' => 'required',
+            'auxtipodocumentoid' => 'required',
+            'numero' => 'required',
+            'numeroconta' => 'required',
+            'numerocontrato' => 'required',
+            'aditivo' => 'required',
+            'valor' => 'required',
+            'numerodocumento' => 'required',
+            'numeronl' => 'required',
+            'historico' => 'required',
         ]);
 
-        // Criação de um novo registro no banco de dados
-        $cadastrar = new \App\Models\Cadastro; 
+        // Criação de um novo registro no banco de dados 
+        $cadastrar = GuiasRecolhimento::create([
+            'auxtiporecolhimentoid' => $request->input('auxtiporecolhimentoid'),
+            'auxinstituicaofinanceiraid' => $request->input('auxinstituicaofinanceiraid'),
+            'auxagenciaid' => $request->input('auxagenciaid'),
+            'auxempresaid' => $request->input('auxempresaid'),
+            'datagr' => $request->input('datagr'),
+            'datavalidade' => $request->input('datavalidade'),
+            'auxtipodocumentoid' => $request->input('auxtipodocumentoid'),
+            'numero' => $request->input('numero'),
+            'numeroconta' => $request->input('numeroconta'),
+            'numerocontrato' => $request->input('numerocontrato'),
+            'aditivo' => $request->input('aditivo'),
+            'valor' => $request->input('valor'),
+            'numerodocumento' => $request->input('numerodocumento'),
+            'numeronl' => $request->input('numeronl'),
+            'historico' => $request->input('historico'),
+        ]);
 
         // Atribuição dos valores dos campos
-        $cadastrar->recolhimento = $request->recolhimento;
-        $cadastrar->financeira = $request->financeira;
-        $cadastrar->agencias = $request->agencias;
-        $cadastrar->contas = $request->contas;
-        $cadastrar->contatos = $request->contatos;
-        $cadastrar->aditivos = $request->aditivos;
-        $cadastrar->datas_grs = $request->datas_grs;
-        $cadastrar->data_validade = $request->data_validade;
-        $cadastrar->documentos = $request->documentos;
-        $cadastrar->numeros = $request->numeros;
-        $cadastrar->empresa = $request->empresa;
-        $cadastrar->valores = $request->valores;
-        $cadastrar->numeros_nls = $request->numeros_nls;
-        $cadastrar->historicos = $request->historicos;
+       
 
         // Salva o registro no banco de dados
         $cadastrar->save();
