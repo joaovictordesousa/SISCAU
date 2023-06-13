2b4766f343df24dc103b747ef4f087588f65ea9d
Rafael Amaral <rafaelosama80@gmail.com>
2023-06-13 11:43:38 -0300

    att

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+        <img src="../img/logonovacap.png">
         </x-slot>
 
         <x-validation-errors class="mb-4" />
