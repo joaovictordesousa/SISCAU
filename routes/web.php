d552d124272e7d292ea2b09616116b8d6ea84b0e
Rafael Amaral <rafaelosama80@gmail.com>
2023-06-13 09:15:06 -0300

    att

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PesquisaController;
 use App\Http\Controllers\CadastroController;
 use App\Http\Controllers\HistoricoController;
+use Illuminate\Support\Facades\Cadastro;
 
 
 /*
@@ -21,28 +22,27 @@ Route::get('/', function () {
     return view('pesquisa');
 });
 
-Route::get('/historico', function (){
-    return view('historico');
-});
+// Route::get('/historico', function (){
+//     return view('historico');
+// });
 
-Route::get('/pesquisa', function (){
-    return view('pesquisa');
-});
+// Route::get('/pesquisa', function (){
+//     return view('pesquisa');
+// });
 
-Route::get('/cadastro', function (){
-    return view('cadastro');
-});
+// Route::get('/cadastro', function (){
+//     return view('cadastro');
+// });
 
-Route::get('/login', function (){
-    return view('login');
-});
+// Route::get('/login', function (){
+//     return view('login');
+// });
 
-Route::get('/cadastro', [PesquisaController::class, 'cadastro'])->name('cadastro'); //Comado de rota pesquisa para cadastro, finaliza no Controller. 
-
-Route::get('/pesquisa', [CadastroController::class, 'pesquisa'])->name('pesquisa');
-
-Route::get('/pesquisa', [HistoricoController::class, 'pesquisa'])->name('pesquisa');
-
-
-
-  
+Route::get('/cadastro', [CadastroController::class, 'cadastro'])->name('cadastro'); //Comado de rota pesquisa para cadastro, finaliza no Controller. 
+//
+Route::get('/pesquisa', [PesquisaController::class, 'pesquisa'])->name('pesquisa');
+//
+Route::get('/historico', [HistoricoController::class, 'historico'])->name('historico');
+//
+Route::post('/cadastro', [CadastroController::class, 'store']);
+//
