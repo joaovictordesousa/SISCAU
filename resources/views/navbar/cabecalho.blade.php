5ad168eef1a89a514a33183d0b1aa12021658bed
joaovictodesousa <joaovictorsr26@gmail.com>
2023-07-14 11:48:42 -0300

    att

---
+++ resources/views/navbar/cabecalho.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../img/logoicon.jpg">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>SISCAU</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <img src="/img/logo_branca.png" alt="logo" class="logo_header">
+        <div class="container_botao">
+        </div> 
+    </header>
